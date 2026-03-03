2e240a6af1c4780c57579c856f65bbcfc2183507
Jeremy R. Groves <jgroves@niu.edu>
2026-03-02 19:08:56 -0600

    fixed MO_data

--- Build/Code/MO data.R
+++ Build/Code/MO data.R
@@ -22,6 +22,8 @@ acs.map <- get_acs(geography = "tract",
 map <- read_sf("./Build/Input/Map/County_Bdy.shp") %>%
   st_transform(., st_crs(acs.map))
 
+bbox <- st_bbox(map)
+
 test <- read.csv(file = "./Build/Input/CrimeData/STLC Crime.csv", header = TRUE, as.is = TRUE)
 
 property <- test %>%
@@ -30,5 +32,18 @@ property <- test %>%
          !is.na(longitude)) %>%
   select(ObjectID, latitude, longitude, occurred) %>%
   mutate(year = as.numeric(substr(occurred, 1, 4))) %>%
-  filter(year > 2019,
-         latitude > 0) 
+  filter(between(longitude, bbox[1], bbox[3]),
+         between(latitude, bbox[2], bbox[4]),
+         year > 2020,
+         year < 2026)
+
+
+prop.map <- st_as_sf(property, coords = c("longitude", "latitude"),
+                     crs = st_crs(map)) %>%
+  mutate(property = 1) %>%
+  st_intersection(., acs.map)
+
+ggplot(prop.map) +
+  geom_sf(aes(fill = property)) +
+  facet_wrap(~year) 
+
